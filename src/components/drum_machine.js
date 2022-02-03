7f77be260f70d0099c2a5dbf4cc18436b7352497
Normaly <40768391+Normaly0@users.noreply.github.com>
2022-02-03 22:02:06 +0100

    Progress 1 Day

--- src/components/drum_machine.js
+++ src/components/drum_machine.js
@@ -2,8 +2,82 @@ import React from 'react';
 
 import styles from './drum_machine.scss';
 
+
 class Drum_Machine extends React.Component {
     render() {
+
+        const sounds = [
+            {
+                value: 'Q',
+                key: 'KeyQ',
+                src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+                name: 'Heater-1'
+            },
+            {
+                value: 'W',
+                key: 'KeyW',
+                src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+                name: 'Heater-2'
+            },
+            {
+                value: 'E',
+                key: 'KeyE',
+                src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+                name: 'Heater-3'
+            },
+            {
+                value: 'A',
+                key: 'KeyA',
+                src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+                name: 'Heater-4'
+            },
+            {
+                value: 'S',
+                key: 'KeyS',
+                src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+                name: 'Clap'
+            },
+            {
+                value: 'D',
+                key: 'KeyD',
+                src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+                name: 'Open-HH'
+            },
+            {
+                value: 'Z',
+                key: 'KeyZ',
+                src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+                name: 'Kick-n\'-Hat'
+            },
+            {
+                value: 'X',
+                key: 'KeyX',
+                src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+                name: 'Kick'
+            },
+            {
+                value: 'C',
+                key: 'KeyC',
+                src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+                name: 'Closed-HH'
+            }
+        ]
+
+        document.addEventListener('keypress', checkKey);
+
+
+        function checkKey(e) {
+            let currentKey = e.code
+
+            if (sounds.some(element => element.key === currentKey)) {
+                let obj = sounds.find(obj => obj.key === currentKey);
+                let sound = new Audio(obj.src);
+                sound.play();
+            }
+            
+        }
+
+
         return (
             <div className = 'drum-panel'>
                 <div className = 'upper-panel'>
@@ -14,15 +88,15 @@ class Drum_Machine extends React.Component {
                 </div>
                 <div className = 'lower-panel'>
                     <div className = 'drum-pad'>
-                        <button type = 'button' value = 'Q'>Q</button>
-                        <button type = 'button' value = 'W'>W</button>
-                        <button type = 'button' value = 'E'>E</button>
-                        <button type = 'button' value = 'A'>A</button>
-                        <button type = 'button' value = 'S'>S</button>
-                        <button type = 'button' value = 'D'>D</button>
-                        <button type = 'button' value = 'Z'>Z</button>
-                        <button type = 'button' value = 'X'>X</button>
-                        <button type = 'button' value = 'C'>C</button>
+                        <button type = 'button' className = 'KeyQ'>Q</button>
+                        <button type = 'button' id = 'KeyW'>W</button>
+                        <button type = 'button' id = 'KeyE'>E</button>
+                        <button type = 'button' id = 'KeyA'>A</button>
+                        <button type = 'button' id = 'KeyS'>S</button>
+                        <button type = 'button' id = 'KeyD'>D</button>
+                        <button type = 'button' id = 'KeyZ'>Z</button>
+                        <button type = 'button' id = 'KeyX'>X</button>
+                        <button type = 'button' id = 'KeyC'>C</button>
                     </div>
                     <div className = 'switch-panel'>
                         <button type = 'button'>Switch</button>
@@ -36,3 +110,4 @@ class Drum_Machine extends React.Component {
 }
 
 export default Drum_Machine;
+
